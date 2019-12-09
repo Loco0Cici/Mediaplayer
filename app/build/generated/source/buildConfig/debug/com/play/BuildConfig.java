9b08dbcf651476d92e171ca91c461d01376260d1
Lishuqi <1471641023@qq.com>
2019-12-09 11:22:50 +0800

    change

--- app/build/generated/source/buildConfig/debug/com/play/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/play/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.sunduoduo.player;
+package com.play;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.sunduoduo.player";
+  public static final String APPLICATION_ID = "com.play";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
